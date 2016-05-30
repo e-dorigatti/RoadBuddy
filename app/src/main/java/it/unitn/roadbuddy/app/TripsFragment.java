1b80698fea4bd083949183c0412773f24b6b363a
BRomans <michele.romani.gzl0@me.com>
2016-05-30 15:52:53 +0300

    Rimossa classe TripsListView ed inserito il contenuto in TripsFragment. Istanziata list_item_trips.xml e ridefinita fragment_trips.xml Inseriti fake data per visualizzare la lista sul telefono

--- app/src/main/java/it/unitn/roadbuddy/app/TripsFragment.java
+++ app/src/main/java/it/unitn/roadbuddy/app/TripsFragment.java
@@ -7,12 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class TripsFragment extends Fragment {
 
-
+    private ArrayAdapter<String> mTripsAdapter;
     public TripsFragment( ) {
         // Required empty public constructor
     }
@@ -26,8 +31,34 @@ public class TripsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trips, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_trips, container, false);
+        String[] data = {
+                "Trip1 - 3 hours - 5stars",
+                "Trip2 - 7 hours - 3stars",
+                "Trip3 - 1 hours - 4stars",
+                "Trip4 - 2 hours - 1stars"
+        };
+
+
+        List<String> tripList = new ArrayList<String>(
+                Arrays.asList(data));
+
+
+        mTripsAdapter = new ArrayAdapter<String>(
+                //current context
+                getActivity(),
+                //ID of list of item layout
+                R.layout.list_item_trips,
+                //ID of the textview to populate
+                R.id.list_item_trips_textview,
+                //forecast data
+                tripList);
+        ListView listView = (ListView) rootView.findViewById(R.id.list_view_trips);
+        listView.setAdapter(mTripsAdapter);
+        return rootView;
     }
+
+
 
     @Override
     public void onAttach(Context context) {
